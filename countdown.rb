ab476753a3520121c9059ad4b616a2368c3db024
Carla Stickler <carlastickler@gmail.com>
2019-04-06 00:08:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,15 @@ def countdown(number)
     "HAPPY NEW YEAR!"
   end
 end
+
+def countdown_with_sleep(number)
+  counter = 0
+  while number > counter
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep 1
+  end
+  if number == 0 
+    "HAPPY NEW YEAR!"
+  end
+end
